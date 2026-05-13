45b792ea88d8cae5cf09efb73c6e9817226c32fd
Lorenzo <lorenzobobeck4535@gmail.com>
2026-05-13 13:54:05 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.src = "./assets/avatar-light.png"
+    img.alt = "Foto de perfil do Mayk Brito sorrindo"
+  } else {
+    img.src = "./assets/avatar.png"
+    img.alt = "Foto de perfil do Mayk Brito sério"
+  }
+}
