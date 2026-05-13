1975f90659650671cce724a0151ba524ff36a61b
Lorenzo <lorenzobobeck4535@gmail.com>
2026-05-13 14:21:25 -0300

    feat: add troca de imagens ao trocar do modo escuro para claro, ou vice-versa

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
     img.src = "./assets/avatar-light.png"
-    img.alt = "Foto de perfil do Mayk Brito sorrindo"
+    img.alt =
+      "Foto de perfil do Mayk Brito sorrindo, com óculos escuros e fundo azul"
   } else {
-    img.src = "./assets/avatar.png"
-    img.alt = "Foto de perfil do Mayk Brito sério"
+    img.src = "./assetss/avatar.png"
+    img.alt = "Foto de perfil do Mayk Brito sério, com óculos e fundo azul"
   }
 }
